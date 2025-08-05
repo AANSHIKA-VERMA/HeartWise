d47aa171c70656011cbe1b7ba478d6f5880ca0cc
Aanshika Verma <142729339+AANSHIKA-VERMA@users.noreply.github.com>
2025-08-05 23:06:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # 1. Import necessary libraries
 import numpy as np
 from flask import Flask, request, jsonify
+from flask_cors import CORS 
 import joblib
 
 # This initializes your backend application.
 app = Flask(__name__)
-
+CORS(app)
 # loads the trained model into memory when the server starts.
 try:
     model = joblib.load('heart_disease_model.joblib')
